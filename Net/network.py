91b0ec0ea66dc5f99bb0ec10acd9cef471d781ee
Vinay Sharma <vinay04sharma@icloud.com>
2018-02-18 19:49:11 +0530

    load model details from yml

--- Net/network.py
+++ Net/network.py
@@ -9,6 +9,7 @@ import zipfile
 import os
 import time
 import threading
+import config
 
 from utils import label_map_util
 
@@ -21,18 +22,32 @@ class Detection_Network():
 	''' Class to create a tensorflow network, based on SSD detection trained on COCO dataset
 	(for the moment). At its creation, it imports the weight from the frozen model.'''
 	def __init__(self):
-		# path to the downloaded model.
-		MODEL_NAME = 'Net/' + 'ssd_mobilenet_v1_coco_2017_11_17'
-		# the class is called from the root dir of the project!
-		MODEL_FILE = MODEL_NAME + '.tar.gz'
 
-		# path to the frozen graph (inside the model).
-		PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
-		# path to the labels (id-name association).
-		PATH_TO_LABELS = os.path.join('Net/data', 'mscoco_label_map.pbtxt')
+		try:
+			cfg = config.load(sys.argv[1])
+		except IndexError:
+			raise SystemExit('Missing YML file. Usage: python2 objectdetector.py objectdetector.yml')
 
-		NUM_CLASSES = 90
+
+		try:
+			# path to the downloaded model.
+			MODEL_NAME = 'Net/' + cfg.getProperty("Model.MODEL_NAME")
+			# the class is called from the root dir of the project!
+			MODEL_FILE = MODEL_NAME + '.tar.gz'
+
+			# path to the frozen graph (inside the model).
+			PATH_TO_CKPT = cfg.getProperty("Model.PATH_TO_CKPT")
+
+			# path to the labels (id-name association).
+			PATH_TO_LABELS = cfg.getProperty("Model.PATH_TO_LABELS")
+
+			NUM_CLASSES = cfg.getProperty("Model.NUM_CLASSES")
+
+		except:
+			raise SystemExit('Incomplete Model Details in Yml file')
+
+
 		# analysing the .tar model.
 		tar_file = tarfile.open(MODEL_FILE)
 		for file in tar_file.getmembers(): # checking if frozen graph exists.
