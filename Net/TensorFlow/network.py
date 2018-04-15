ae7821d6b640e0d09047a268c8fc4d31cb040957
Nacho Condés <nachocmstrato@gmail.com>
2018-04-15 20:01:09 +0200

    Finished complete and transparent integration of Keras. Closing #21

--- Net/TensorFlow/network.py
+++ Net/TensorFlow/network.py
@@ -45,8 +45,11 @@ class DetectionNetwork():
 
         label_map = label_map_util.load_labelmap(('Net/TensorFlow/labels/' + LABELS)) # loads the labels map.
         categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES)
-        self.category_index = label_map_util.create_category_index(categories)
-
+        category_index = label_map_util.create_category_index(categories)
+        self.classes = {}
+        # We build is as a dict because of gaps on the labels definitions
+        for cat in category_index:
+            self.classes[cat] = str(category_index[cat]['name'])
 
         self.sess = tf.Session(graph=detection_graph)
         self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -64,15 +67,34 @@ class DetectionNetwork():
                 [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
                 feed_dict={self.image_tensor: dummy_tensor})
 
+        self.confidence_threshold = 0.5
+
         print("Network ready!")
 
     def setCamera(self, cam):
         self.cam = cam
+        self.original_height = cam.im_height
+        self.original_width = cam.im_width
 
 
     def predict(self):
         input_image = self.cam.getImage()
         image_np_expanded = np.expand_dims(input_image, axis=0)
-        (self.boxes, self.scores, self.classes, self.num) = self.sess.run(
+        (boxes, scores, predictions, _) = self.sess.run(
             [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
             feed_dict={self.image_tensor: image_np_expanded})
+
+        # We only keep the most confident predictions.
+        conf = scores > self.confidence_threshold # bool array
+        boxes = boxes[conf]
+        # aux variable for avoiding race condition while int casting
+        tmp_boxes = np.zeros([len(boxes), 4])
+        tmp_boxes[:,[0,2]] = boxes[:,[1,3]] * self.original_width
+        tmp_boxes[:,[3,1]] = boxes[:,[2,0]] * self.original_height
+        self.boxes = tmp_boxes.astype(int)
+
+        self.scores = scores[conf]
+        predictions = predictions[conf].astype(int)
+        self.predictions = []
+        for pred in predictions:
+            self.predictions.append(self.classes[pred])
